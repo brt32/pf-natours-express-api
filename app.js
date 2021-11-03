ef1bba6dd7362915fe08454fc2c68443b6ae3a74
Bartek <bartlomiej.proczkowski@gmail.com>
2021-11-03 14:41:11 +0100

    CORS implemented

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require("xss-clean");
 const hpp = require("hpp");
 const cookieParser = require("cookie-parser");
 const compression = require("compression");
+const cors = require("cors");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -26,6 +27,15 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 // 1) GLOBAL MIDDLEWARES
+app.use(cors()); // Access-Control-Allow-Origin * for everyone
+// app.use(
+//   cors({
+//     origin: "https://api.natours.com",
+//   })
+// ); // for specific origin
+
+app.options("*", cors()); // for get, create, put, patch, delete * for everyone
+
 app.use(helmet()); // Set security HTTP headers
 
 // Development logging
