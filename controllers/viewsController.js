ea3eb0c202311467b8f39e1c22b4a9ec01d8b635
Bartek <bartlomiej.proczkowski@gmail.com>
2021-11-03 16:06:49 +0100

    Javascript Bundled

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -4,6 +4,15 @@ const User = require("./../models/userModel");
 const AppError = require("./../utils/appError");
 const catchAsync = require("./../utils/catchAsync");
 
+exports.alerts = (req, res, next) => {
+  const { alert } = req.query;
+  if (alert === "booking") {
+    res.locals.alert =
+      "Your booking was successfull! Please check your email for a confirmation.";
+  }
+  next();
+};
+
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
   tours = await Tour.find();
