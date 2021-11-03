6bab1ba9c01af534731d08d206c5774862fc31f0
Bartek <bartlomiej.proczkowski@gmail.com>
2021-11-03 15:29:15 +0100

    Stripe Implementation

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const hpp = require("hpp");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const compression = require("compression");
 const cors = require("cors");
 
@@ -17,6 +18,8 @@ const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
 const viewRouter = require("./routes/viewRoutes");
 const bookingRouter = require("./routes/bookingRoutes");
+
+const bookingController = require("./controllers/bookingController");
 
 const app = express();
 
@@ -51,6 +54,8 @@ const limiter = rateLimit({
 });
 
 app.use("/api", limiter);
+
+app.post("/webhook-checkout", bookingController.webhookCheckout); // Stripe not working with JSON, only with RAW format
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
